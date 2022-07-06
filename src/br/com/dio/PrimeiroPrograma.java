2ec9077bea1637bb68ca231a67599b61606d8f3a
Jerrayne Rodrigues <jerrayner@gmail.com>
2022-07-06 15:56:51 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		
+		
+		System.out.println(gato);
+		
+		
+	}
+}
+
